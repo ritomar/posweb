85f699e980b88c384bb543d8f7e3049a7643346b
Ritomar Torquato <ritomar@hotmail.com>
2016-10-30 12:07:27 -0300

    Adiciona Template

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,5 +1,9 @@
 ﻿from django.shortcuts import render
-from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Alô, mundo!")
+    contexto = { "titulo": "Instituto Federal do Piauí",
+                 "subtitulo" : "Turma 2016",
+                 "menu" : ['home', 'sobre', 'porfólio', 'produtos', 'FAQ', 'contato']}
+
+    return render(request, 'index.html', contexto)
